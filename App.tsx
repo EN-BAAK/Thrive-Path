5d99aa0a6c8292246608f506839a863185ce06fc
EN-BAAK <swaiengineer@gmail.com>
2025-07-10 16:04:40 +0300

    create project structure

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Hello world</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
